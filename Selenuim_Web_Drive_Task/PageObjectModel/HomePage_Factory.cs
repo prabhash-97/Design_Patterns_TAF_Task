69aab544db8455436458bbda647bdb2a7edc9926
Kasuni <kasunirashmika568@gmail.com>
2022-11-04 16:22:55 +0530

    initial commit

---
+++ Selenuim_Web_Drive_Task/PageObjectModel/HomePage_Factory.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeleniumExtras.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using Selenuim_Web_Drive_Task.Decorator;
+using Selenuim_Web_Drive_Task.Singletone;
+
+namespace Selenuim_Web_Drive_Task.PageObjects
+{
+    public class HomePage_Factory
+    {
+        string testUrl = "http://demo.guru99.com/test/guru99home/";
+
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        public HomePage_Factory(IWebDriver driver)
+        {
+            this.driver = driver;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(50));
+            PageFactory.InitElements(driver, this);
+        }
+
+        private IWebElement emailTextBox => driver.FindElement(By.CssSelector("input[id=philadelphia-field-email]"));
+        private IWebElement signUpButton => driver.FindElement(By.XPath(".//*[@id='philadelphia-field-submit']"));
+
+        public void GoToPage()
+        {
+            driver.Navigate().GoToUrl(testUrl);
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
+        }
+
+        public void Signup()
+        {
+            emailTextBox.Clear();
+            emailTextBox.SendKeys("test123@gmail.com");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
+            signUpButton.Click();
+        }
+
+        public void IsSignupSuccessful()
+        {
+            var expectedAlertText = "Form Submitted Successfully...";
+            var alert_win = driver.SwitchTo().Alert();
+
+            Assert.AreEqual(expectedAlertText, alert_win.Text);
+            alert_win.Accept();
+        }
+    }
+}
---
+++ Selenuim_Web_Drive_Task/Test/TestClass_Factory.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using Selenuim_Web_Drive_Task.PageObjects;
+using System;
+
+namespace Selenuim_Web_Drive_Task
+{
+    public class TestClass_Factory
+    {
+        IWebDriver webDriver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            WebDriverFactory factory = new WebDriverFactory();
+            webDriver = factory.CreateInstance("Chrome");
+            HomePage_Factory home_page = new HomePage_Factory(webDriver);
+            home_page.GoToPage();
+        }
+
+        [Test]
+        public void Test_Factory()
+        {
+            HomePage_Factory home_page = new HomePage_Factory(webDriver);
+            //act
+            home_page.Signup();
+            //assert
+            home_page.IsSignupSuccessful();
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            webDriver.Quit(); 
+        }
+
+    }
+}
